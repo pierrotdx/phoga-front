6775b102c8ac63868d84068338e72bf7c8c4007c
pierrotdx <pierrotdx@gmail.com>
2025-01-29 17:37:51 +0100

    update environments

--- projects/back-office/src/environment-context/core/environment.staging.ts
+++ projects/back-office/src/environment-context/core/environment.staging.ts
@@ -1,7 +1,7 @@
 import { IEnvironment } from './models/environment';
 
 export const environment: IEnvironment = {
-  phogaApiUrl: 'https://phoga-staging-api-84144217271.europe-west9.run.app/',
+  phogaApiUrl: 'https://phoga-api-staging-99420973827.europe-west9.run.app',
   auth0Config: {
     domain: 'phoga-staging.eu.auth0.com',
     clientId: 'Ytn906GeMz2CTCfg2LC024iSoUbNPEXk',
