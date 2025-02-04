637918a8b6bc352560c3749cbafe60be2a600106
pierrotdx <pierrotdx@gmail.com>
2025-02-04 19:24:09 +0100

    collage view

--- projects/client/src/app/components/photo-card/photo-card.component.ts
+++ projects/client/src/app/components/photo-card/photo-card.component.ts
@@ -1,14 +1,28 @@
 import { AsyncPipe } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
 import { IPhoto } from '@shared/photo-context';
 import { BufferToImagePipe } from '@shared/pipes';
 import { PhotoMetadataComponent } from './photo-metadata/photo-metadata.component';
+import { PhotoFullscreenComponent } from './photo-fullscreen/photo-fullscreen.component';
+import { MaterialIconComponent } from '@shared/material-icon';
 
 @Component({
   selector: 'app-photo-card',
-  imports: [BufferToImagePipe, AsyncPipe, PhotoMetadataComponent],
+  imports: [
+    BufferToImagePipe,
+    AsyncPipe,
+    PhotoMetadataComponent,
+    MaterialIconComponent,
+    PhotoFullscreenComponent,
+  ],
   templateUrl: './photo-card.component.html',
 })
-export class PhotoCardComponent  {
+export class PhotoCardComponent {
   @Input() photo!: IPhoto;
+  readonly isFullscreen = signal<boolean>(false);
+
+  toggleFullscreen() {
+    const isFullscreen = this.isFullscreen();
+    this.isFullscreen.set(!isFullscreen);
+  }
 }
