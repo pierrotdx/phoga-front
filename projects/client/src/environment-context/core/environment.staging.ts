b667152a386f6f9a0b31f88c6281e45b35dcc055
pierrotdx <pierrotdx@gmail.com>
2025-02-10 16:48:41 +0100

    fix build

--- projects/client/src/environment-context/core/environment.staging.ts
+++ projects/client/src/environment-context/core/environment.staging.ts
@@ -3,17 +3,16 @@ import { IEnvironment } from './models/environment';
 export const environment: IEnvironment = {
   phogaApiUrl: 'https://phoga-api-staging-99420973827.europe-west9.run.app',
   mainContact: {
-    mainContact: {
-      name: 'Pierre',
-      description: 'photography, website',
-      email: 'pierrotdx@gmail.com',
-      github: 'pierrotdx',
-      instagram: 'noLink',
-    },
-    designContact: {
-      name: 'Prachi',
-      description: 'design',
-      email: 'prachi@nomail.com',
-      behance: 'nolink',
-    },
+    name: 'Pierre',
+    description: 'photography, website',
+    email: 'pierrotdx@gmail.com',
+    github: 'pierrotdx',
+    instagram: 'noLink',
+  },
+  designContact: {
+    name: 'Prachi',
+    description: 'design',
+    email: 'prachi@nomail.com',
+    behance: 'nolink',
+  },
 };
