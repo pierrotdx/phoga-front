f579e74d45ba10caadcbe13952b95541cb1ef7a5
pierrotdx <pierrotdx@gmail.com>
2025-02-17 18:35:06 +0100

    update contact infos

--- projects/client/src/environment-context/core/environment.ts
+++ projects/client/src/environment-context/core/environment.ts
@@ -3,16 +3,11 @@ import { IEnvironment } from './models/environment';
 export const environment: IEnvironment = {
   phogaApiUrl: 'http://localhost:3000',
   mainContact: {
-    name: 'Pierre',
-    description: 'photography, website',
     email: 'pierrotdx@gmail.com',
-    github: 'pierrotdx',
+    github: 'https://github.com/pierrotdx',
     instagram: 'noLink',
   },
   designContact: {
-    name: 'Prachi',
-    description: 'design',
-    email: 'prachi@nomail.com',
-    behance: 'nolink',
+    behance: 'https://www.behance.net/prachikarkun',
   },
 };
