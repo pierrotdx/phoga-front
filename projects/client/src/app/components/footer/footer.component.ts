7da276a922a6dbca1c5614013859484971465ae3
pierrotdx <pierrotdx@gmail.com>
2025-02-17 18:17:38 +0100

    update footer view

--- projects/client/src/app/components/footer/footer.component.ts
+++ projects/client/src/app/components/footer/footer.component.ts
@@ -1,15 +1,22 @@
 import { Component, Inject, signal } from '@angular/core';
 import { ENVIRONMENT_TOKEN, IEnvironment } from '@client/environment-context';
-import { ContactComponent, IContact } from '@shared/contact-context';
+import { IContact } from '@shared/contact-context';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
-  imports: [ContactComponent],
+  imports: [FontAwesomeModule],
   templateUrl: './footer.component.html',
 })
 export class FooterComponent {
   mainContact = signal<IContact | undefined>(undefined);
   designContact = signal<IContact | undefined>(undefined);
+
+  envelopIcon = faEnvelope;
+  githubIcon = faGithub;
+  instagramIcon = faInstagram;
 
   constructor(@Inject(ENVIRONMENT_TOKEN) private readonly env: IEnvironment) {
     const mainContact = this.env?.mainContact;
