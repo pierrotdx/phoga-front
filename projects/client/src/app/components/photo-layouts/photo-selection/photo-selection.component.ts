bf1991fb64bd443fc1a3348f9f6caaa71013b1dd
pierrotdx <pierrotdx@gmail.com>
2025-03-06 17:04:31 +0100

    update photo-selection cmpt to use photo-selector service

--- projects/client/src/app/components/photo-layouts/photo-selection/photo-selection.component.ts
+++ projects/client/src/app/components/photo-layouts/photo-selection/photo-selection.component.ts
@@ -1,6 +1,18 @@
-import { Component, Input, model } from '@angular/core';
-import { IPhoto } from '@shared/photo-context';
+import {
+  Component,
+  Inject,
+  Input,
+  model,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import {
+  IPhoto,
+  IPhotoSelector,
+  PHOTO_SELECTOR_SERVICE_TOKEN,
+} from '@shared/photo-context';
 import { PhotoImageComponent } from '../photo-image/photo-image.component';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-photo-selection',
@@ -8,20 +20,37 @@ import { PhotoImageComponent } from '../photo-image/photo-image.component';
   templateUrl: './photo-selection.component.html',
   styleUrl: './photo-selection.component.scss',
 })
-export class PhotoSelectionComponent {
+export class PhotoSelectionComponent implements OnInit, OnDestroy {
   @Input() photos: IPhoto[] = [];
-  selectedPhoto = model<IPhoto | undefined>(undefined);
+  readonly selectedPhoto = model<IPhoto | undefined>(undefined);
+  private readonly subs: Subscription[] = [];
 
-  togglePhotoSelection(id: IPhoto['_id']): void {
-    if (id === this.selectedPhoto()?._id) {
-      this.resetSelection();
-    } else {
-      const photo = this.photos.find((p) => p._id === id);
-      this.selectedPhoto.set(photo);
-    }
+  constructor(
+    @Inject(PHOTO_SELECTOR_SERVICE_TOKEN)
+    private readonly photoSelectorService: IPhotoSelector
+  ) {}
+
+  ngOnInit(): void {
+    this.subToSelectedPhoto();
   }
 
-  private resetSelection(): void {
-    this.selectedPhoto.set(undefined);
+  ngOnDestroy(): void {
+    this.subs.forEach((sub) => sub.unsubscribe());
+  }
+
+  private subToSelectedPhoto(): void {
+    const sub = this.photoSelectorService.selectedPhoto.subscribe(
+      (selectedPhoto) => this.onSelectedPhotoChange(selectedPhoto)
+    );
+    this.subs.push(sub);
+  }
+
+  private onSelectedPhotoChange(photo: IPhoto | undefined): void {
+    this.selectedPhoto.set(photo);
+  }
+
+  selectPhoto(id: IPhoto['_id']): void {
+    const photo = this.photos.find((p) => p._id === id);
+    this.photoSelectorService.selectedPhoto.next(photo);
   }
 }
