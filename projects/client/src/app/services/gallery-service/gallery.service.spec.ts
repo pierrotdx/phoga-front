04ce8d4f2eb8169d63efe8732397d8ab0f932fef
pierrotdx <pierrotdx@gmail.com>
2025-03-25 16:08:03 +0100

    add gallery service

---
+++ projects/client/src/app/services/gallery-service/gallery.service.spec.ts
@@ -0,0 +1,224 @@
+import { IPhoto } from '@shared/photo-context';
+import { GalleryServiceTestUtils } from './gallery.test-utils';
+import { GalleryService } from './gallery.service';
+import { Subscription } from 'rxjs';
+
+describe('GalleryService', () => {
+  let testUtils: GalleryServiceTestUtils;
+  let galleryService: GalleryService;
+
+  beforeEach(() => {
+    testUtils = new GalleryServiceTestUtils();
+    testUtils.globalBeforeEach();
+    galleryService = testUtils.getService();
+  });
+
+  it('should be created', () => {
+    testUtils.expectServiceToBeCreated();
+  });
+
+  describe('photos', () => {
+    it('should be initialized to an empty array', () => {
+      const expectedPhotos: IPhoto[] = [];
+      testUtils.expectServicePhotosToBe(expectedPhotos);
+    });
+  });
+
+  describe('loadMore', () => {
+    let stubLoadedPhotos: IPhoto[];
+    let size: number;
+    let defaultSize: number;
+
+    beforeEach(() => {
+      defaultSize = testUtils.getService()['defaultSize'];
+      size = defaultSize;
+      stubLoadedPhotos = testUtils.getLoadedPhotosBatch(0, size);
+      testUtils.stubSearchPhotoResponse(stubLoadedPhotos);
+    });
+
+    it('should return loaded photos', async () => {
+      const loadedPhotos = await galleryService.loadMore();
+      expect(loadedPhotos).toEqual(stubLoadedPhotos);
+    });
+
+    it("should update the value of service's photos", async () => {
+      let expectedPhotos: IPhoto[] = [];
+      testUtils.expectServicePhotosToBe(expectedPhotos);
+      testUtils.expectServicePhotosLengthToBe(0);
+
+      let loadedPhotos: IPhoto[];
+      const batchSize = defaultSize;
+      for (let batchNb = 1; batchNb < 3; batchNb++) {
+        const batchStart = (batchNb - 1) * batchSize;
+        stubLoadedPhotos = testUtils.getLoadedPhotosBatch(
+          batchStart,
+          batchSize
+        );
+        testUtils.stubSearchPhotoResponse(stubLoadedPhotos);
+
+        loadedPhotos = await galleryService.loadMore();
+
+        expectedPhotos = expectedPhotos.concat(loadedPhotos);
+        testUtils.expectServicePhotosToBe(expectedPhotos);
+        // for simplicity we consider case where the returned batches are full (= batchSize)
+        testUtils.expectServicePhotosLengthToBe(batchNb * batchSize);
+      }
+    });
+
+    describe('request', () => {
+      it('should request the next photo batch based on the previous one', async () => {
+        testUtils.stubSearchPhotoResponse(stubLoadedPhotos);
+
+        let expectedFrom = 0;
+        let loadedPhotos = await galleryService.loadMore();
+
+        testUtils.expectPhotoRequestFromToBe(expectedFrom);
+
+        expectedFrom += loadedPhotos.length + 1;
+        await galleryService.loadMore();
+
+        testUtils.expectPhotoRequestFromToBe(expectedFrom);
+      });
+
+      describe("response's size", () => {
+        it(`should be of the service\'s default size`, async () => {
+          const expectedSize = defaultSize;
+
+          await galleryService.loadMore();
+
+          testUtils.expectPhotoRequestSizeToBe(expectedSize);
+        });
+
+        it('should match the input size', async () => {
+          const expectedSize = 4;
+
+          await galleryService.loadMore(expectedSize);
+
+          testUtils.expectPhotoRequestSizeToBe(expectedSize);
+        });
+      });
+
+      it('should not send the request when there is no more photos to load', async () => {
+        size = 3;
+        // last batch because batch-size < size
+        const lastPhotoBatch = stubLoadedPhotos.slice(0, size - 1);
+        testUtils.stubSearchPhotoResponse(lastPhotoBatch);
+
+        const loadedPhotos = await galleryService.loadMore(size);
+        testUtils.expectServicePhotosToBe(loadedPhotos);
+        await galleryService.loadMore(size);
+
+        const requestSpy = testUtils.getSearchPhotoSpy();
+        expect(requestSpy).toHaveBeenCalledTimes(1);
+      });
+    });
+
+    describe('isLoading', () => {
+      let isLoadingSub: Subscription;
+      const isLoadingValues: boolean[] = [];
+      const addIsLoadingValue = (value: boolean) => isLoadingValues.push(value);
+
+      beforeEach(() => {
+        const isLoading$ = testUtils.getService().isLoading$;
+        isLoadingSub = isLoading$.subscribe(addIsLoadingValue);
+      });
+
+      afterEach(() => {
+        isLoadingSub.unsubscribe();
+      });
+
+      it('should emit true when the loading is starting', async () => {
+        await galleryService.loadMore();
+        expect(isLoadingValues[0]).toBeTrue();
+      });
+
+      it('should emit false when the loading returns photos', async () => {
+        await galleryService.loadMore();
+        expect(isLoadingValues[1]).toBeFalse();
+      });
+
+      it('should emit false in case of loading error', async () => {
+        const stubError = new Error('stub error');
+        testUtils.stubSearchPhotoResponse(stubError);
+        try {
+          await galleryService.loadMore();
+        } catch (err) {
+          expect(err).toEqual(stubError);
+        } finally {
+          expect(isLoadingValues[1]).toBeFalse();
+        }
+      });
+    });
+  });
+
+  describe('selectPhoto', () => {
+    beforeEach(() => {
+      const stubPhotos = testUtils.allStubPhotos;
+      testUtils.stubServicePhotos(stubPhotos);
+    });
+
+    it('should select the input photo', () => {
+      const expectedPhoto = testUtils.allStubPhotos[0];
+
+      galleryService.selectPhoto(expectedPhoto._id);
+
+      testUtils.expectSelectedPhotoToBe(expectedPhoto._id);
+    });
+
+    it("should leave the selection unchanged if the input photo is not part of the service's photos", () => {
+      const selectedPhoto = testUtils.allStubPhotos[0];
+      galleryService.selectPhoto(selectedPhoto._id);
+
+      const inexistantPhotoId = 'some id';
+      galleryService.selectPhoto(inexistantPhotoId);
+
+      testUtils.expectSelectedPhotoToBe(selectedPhoto._id);
+    });
+  });
+
+  describe('deselectPhoto', () => {
+    let selectedPhoto: IPhoto | undefined;
+
+    beforeEach(() => {
+      const stubPhotos = testUtils.allStubPhotos;
+      testUtils.stubServicePhotos(stubPhotos);
+
+      selectedPhoto = testUtils.allStubPhotos[0];
+      galleryService.selectPhoto(selectedPhoto._id);
+    });
+
+    it('should deselect the selected photo', () => {
+      expect(selectedPhoto?._id).toBeDefined();
+      testUtils.expectSelectedPhotoToBe(selectedPhoto!._id);
+
+      galleryService.deselectPhoto();
+
+      testUtils.expectSelectedPhotoToBe(undefined);
+    });
+  });
+
+  describe('selectedPhoto$', () => {
+    beforeEach(() => {
+      const stubPhotos = testUtils.allStubPhotos;
+      testUtils.stubServicePhotos(stubPhotos);
+    });
+
+    it('should be `undefined` by default', () => {
+      const selectedPhoto = testUtils.getSelectedPhoto();
+      expect(selectedPhoto).toBeUndefined();
+    });
+
+    it('should change according to (de)selection ', () => {
+      let selectedPhoto = testUtils.getSelectedPhoto();
+
+      expect(selectedPhoto).toBeUndefined();
+
+      const expectedSelectedPhoto = testUtils.allStubPhotos[0];
+      galleryService.selectPhoto(expectedSelectedPhoto._id);
+      testUtils.expectSelectedPhotoToBe(expectedSelectedPhoto._id);
+
+      galleryService.deselectPhoto();
+      testUtils.expectSelectedPhotoToBe(undefined);
+    });
+  });
+});
---
+++ projects/client/src/app/services/gallery-service/gallery.service.ts
@@ -0,0 +1,106 @@
+import { Injectable } from '@angular/core';
+import {
+  IPhoto,
+  ISearchPhotoOptions,
+  PhotoApiService,
+} from '@shared/photo-context';
+import { BehaviorSubject, firstValueFrom, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GalleryService {
+  readonly photos$ = new BehaviorSubject<IPhoto[]>([]);
+  readonly isLoading$ = new Subject<boolean>();
+
+  private from: number = 0;
+  private defaultSize: number = 2;
+  private hasMoreToLoad: boolean = true;
+
+  readonly selectedPhoto$ = new BehaviorSubject<IPhoto | undefined>(undefined);
+
+  constructor(private readonly photoApiService: PhotoApiService) {}
+
+  async loadMore(size?: number): Promise<IPhoto[]> {
+    if (!this.hasMoreToLoad) {
+      return [];
+    }
+    this.isLoading$.next(true);
+    try {
+      const searchPhotoOptions = this.getSearchPhotoOptions(size);
+      const loadedPhotos = await this.fetchPhotos(searchPhotoOptions);
+      this.onPhotoLoading(searchPhotoOptions, loadedPhotos);
+      return loadedPhotos;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    } finally {
+      this.isLoading$.next(false);
+    }
+  }
+
+  private getSearchPhotoOptions(size?: number): ISearchPhotoOptions {
+    const options: ISearchPhotoOptions = {
+      rendering: {
+        from: this.from,
+        size: size || this.defaultSize,
+      },
+    };
+    return options;
+  }
+
+  private fetchPhotos = async (searchPhotoOptions?: ISearchPhotoOptions) => {
+    const loadRequest$ = this.photoApiService.searchPhoto(searchPhotoOptions);
+    const result = await firstValueFrom(loadRequest$);
+    if (result instanceof Error) {
+      throw result;
+    }
+    return result;
+  };
+
+  private onPhotoLoading(
+    searchPhotoOptions: ISearchPhotoOptions,
+    loadedPhotos: IPhoto[]
+  ): void {
+    if (loadedPhotos.length > 0) {
+      this.updatePhotos(loadedPhotos);
+      this.updateFrom(loadedPhotos.length + 1);
+    }
+    this.updateHasMoreToLoad(searchPhotoOptions, loadedPhotos);
+  }
+
+  private updateFrom(step: number): void {
+    this.from += step;
+  }
+
+  private updatePhotos(photosToAdd: IPhoto[]): void {
+    const photos = this.photos$.getValue();
+    const newPhotos = photos.concat(photosToAdd);
+    this.photos$.next(newPhotos);
+  }
+
+  private updateHasMoreToLoad(
+    searchPhotoOptions: ISearchPhotoOptions,
+    loadedPhotos: IPhoto[]
+  ): void {
+    this.hasMoreToLoad =
+      loadedPhotos.length === searchPhotoOptions.rendering!.size;
+  }
+
+  selectPhoto(id: IPhoto['_id']): void {
+    const selectedPhoto = this.findPhoto(id);
+    if (!selectedPhoto) {
+      return;
+    }
+    this.selectedPhoto$.next(selectedPhoto);
+  }
+
+  private findPhoto(photoId: IPhoto['_id']): IPhoto | undefined {
+    const photos = this.photos$.getValue();
+    return photos.find((p) => p._id === photoId);
+  }
+
+  deselectPhoto(): void {
+    this.selectedPhoto$.next(undefined);
+  }
+}
